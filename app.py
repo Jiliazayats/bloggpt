b9566f9d86ac22934fb98fd8a9fcc00852e96f55
Jiliazayats <jiliazayats@gmail.com>
2024-11-19 20:21:13 +0300

    app.py

--- app.py
+++ app.py
@@ -95,7 +95,5 @@ async def heartbeat_api():
 
 if __name__ == "__main__":
     import uvicorn
-    import os
-    port = int(os.environ.get("PORT", 8000))  # Render задает порт через переменную PORT
-    uvicorn.run("app:app", host="0.0.0.0", port=port)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000)
 
