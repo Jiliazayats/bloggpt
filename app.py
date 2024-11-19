64658385b67cd10102c0a65d47043d76127a69c1
Jiliazayats <jiliazayats@gmail.com>
2024-11-19 17:07:04 +0300

    app.py

--- app.py
+++ app.py
@@ -95,4 +95,4 @@ async def heartbeat_api():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8000)
+    uvicorn.run("app:app", host="0.0.0.0", port=3000)
